e60b66e90c4a18c745d92b936ec72eb604b7e48e
Chi Truong <vtchitruong@gmail.com>
2021-09-11 21:00:58 +0700

    The frequency of occurrence of string array

---
+++ MatchString/matchstring.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+#define inputFile "matchstring1.inp"
+#define outputFile "matchstring1.out"
+
+using namespace std;
+
+int n;
+vector<string> strings;
+
+int q; // number of queries to be matched
+vector<string> query;
+
+void Input()
+{
+    ifstream f;
+    f.open(inputFile);
+
+    f >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        string tmp;
+        f >> tmp;
+        strings.push_back(tmp);
+    }
+
+    f >> q;
+
+    for (int i = 0; i < q; ++i)
+    {
+        string tmp;
+        f >> tmp;
+        query.push_back(tmp);
+    }
+
+    f.close();
+}
+
+vector<int> Process()
+{
+    map<string, int> m;
+
+    for (int i = 0; i < strings.size(); ++i)
+    {
+        m[strings[i]]++;
+    }
+
+    vector<int> res; // result of matching
+    for (int i = 0; i < query.size(); ++i)
+    {
+        res.push_back(m[query[i]]);
+    }
+
+    return res;
+}
+
+void Output()
+{
+    vector<int> result = Process();
+
+    ofstream f;
+    f.open(outputFile);
+
+    for (int i = 0; i < result.size(); ++i)
+    {
+        f << result[i];
+        if (i != result.size() - 1) f << ' ';
+    }
+    
+    f.close();
+}
+
+int main()
+{
+    Input();
+    Output();
+
+    return 0;
+}
